9a9a901d7f4815f2fa4d607fe0d4772e908f50aa
c-abbott <cllmbbtt@gmail.com>
2023-05-22 21:50:57 +0200

    Using proxy backend to display ExpenseList

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:big_spender/model/expense.dart';
+import 'package:big_spender/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,15 +12,26 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Groceries',
+        amount: 20.0,
+        expenseCreatedAt: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'Cinema',
+        amount: 14.30,
+        expenseCreatedAt: DateTime.now(),
+        category: Category.leisure)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [
-          Text('The chart'),
-          Text(
-            'Expenses list',
-          )
+        children: [
+          const Text('The chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses))
         ],
       ),
     );
---
+++ lib/expenses_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:big_spender/model/expense.dart';
+
+class ExpensesList extends StatelessWidget {
+  const ExpensesList({Key? key, required this.expenses}) : super(key: key);
+
+  final List<Expense> expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) => Text(expenses[index].title),
+    );
+  }
+}
