32c15041d2444369821cbe0b8f8ea2f60e14dad9
c-abbott <cllmbbtt@gmail.com>
2023-05-23 21:53:30 +0200

    Added floatingActionButton :heavy_plus_sign:

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -3,7 +3,7 @@ import 'package:big_spender/model/expense.dart';
 import 'package:big_spender/widgets/expenses_list/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
-  const Expenses({super.key});
+  const Expenses({Key? key}) : super(key: key);
 
   @override
   State<Expenses> createState() {
@@ -25,14 +25,26 @@ class _ExpensesState extends State<Expenses> {
         category: Category.leisure)
   ];
 
+  void _addExpense() {
+    // TODO: Add your code to navigate to add expense screen or open a dialog here
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Big Spender'),
+      ),
       body: Column(
         children: [
           const Text('The chart'),
           Expanded(child: ExpensesList(expenses: _registeredExpenses))
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addExpense,
+        tooltip: 'Add Expense',
+        child: Icon(Icons.add),
       ),
     );
   }
