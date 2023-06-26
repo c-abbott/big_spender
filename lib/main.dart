74ce2ff3883bbb8b1511e95d93e9ed0f1e2245d5
c-abbott <cllmbbtt@gmail.com>
2023-06-26 18:54:40 +0000

    Only allowing portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:big_spender/widgets/expenses.dart';
 
 var kColorScheme =
@@ -8,36 +9,41 @@ var kDarkColorScheme = ColorScheme.fromSeed(
     brightness: Brightness.dark, seedColor: Colors.black87);
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark()
-          .copyWith(useMaterial3: true, colorScheme: kDarkColorScheme),
-      theme: ThemeData().copyWith(
-        useMaterial3: true,
-        colorScheme: kColorScheme,
-        cardTheme: const CardTheme().copyWith(
-          color: Colors.black87,
-          margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((fn) {
+    runApp(
+      MaterialApp(
+        darkTheme: ThemeData.dark()
+            .copyWith(useMaterial3: true, colorScheme: kDarkColorScheme),
+        theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          cardTheme: const CardTheme().copyWith(
+            color: Colors.black87,
+            margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
           ),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-              titleMedium: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Proxima Nova'),
-              bodySmall: const TextStyle(
-                fontSize: 12,
-                fontWeight: FontWeight.normal,
-                fontFamily: 'Proxima Nova',
-              ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
             ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleMedium: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Proxima Nova'),
+                bodySmall: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.normal,
+                  fontFamily: 'Proxima Nova',
+                ),
+              ),
+        ),
+        //themeMode: ThemeMode.dark,
+        home: const Expenses(),
       ),
-      themeMode: ThemeMode.dark,
-      home: const Expenses(),
-    ),
-  );
+    );
+  });
 }
